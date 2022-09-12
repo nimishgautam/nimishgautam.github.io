9bb8c10c70dbb78a083d166b665ac2c1b8e85ce2
Nimish Gåtam <nimishg@gmail.com>
2022-09-12 19:20:31 +0200

    comments active

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220912.185754';
+const cacheName = 'chirpy-20220912.191918';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
