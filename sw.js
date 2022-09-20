e9abc82710279eaeb8f4be20e9d3dd7285eafaee
Nimish Gåtam <nimishg@gmail.com>
2022-09-20 10:59:10 +0200

    portege

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220912.191918';
+const cacheName = 'chirpy-20220920.105616';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
