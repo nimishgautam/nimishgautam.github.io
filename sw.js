d54039d03eb7548b1f07c2a83ed7df94019b4cd9
Nimish Gåtam <nimishg@gmail.com>
2022-11-10 17:42:40 +0100

    founder article

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220921.233344';
+const cacheName = 'chirpy-20221110.174014';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
